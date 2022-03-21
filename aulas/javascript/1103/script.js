fde0b95b3fc330d28edf9186e93d4bb29de0967d
glauber_costa14@yahoo.com.br <73006247+glaubercsouza@users.noreply.github.com>
2022-03-21 20:07:43 -0300

    1103

---
+++ aulas/javascript/1103/script.js
@@ -0,0 +1,25 @@
+// String
+// const text = 'JavaScript';
+// const frase = ' Isso é o meu "jogo"'
+
+// console.log(text + frase);
+
+// ----------------
+// Número
+let total = 0;
+
+const compras = 30;
+const imposto = 10 ;
+
+total = compras + imposto;
+
+const dobro = total * 2;
+const dividir = total / 2;
+const desconto = total - 30;
+
+const strings = '20' + '20';
+const numbers = 20 + 20;
+
+const transformar = Number("20") + 20;
+
+console.log(20.5);
