3b885590759db55b8fccb1e499ac9c42c731c291
glauber_costa14@yahoo.com.br <73006247+glaubercsouza@users.noreply.github.com>
2022-03-23 19:14:20 -0300

    eventos

---
+++ aulas/javascript/1106-eventos/script.js
@@ -0,0 +1,22 @@
+// == Métodos ==
+// const curso = {
+//   nome: "JavaScript",
+//   completar() {
+//     console.log(this.nome);
+//   }
+// }
+
+// curso.completar();
+
+// == Eventos ==
+const ativar = document.querySelector('.ativar');
+
+
+/** @param {MouseEvent} event */
+function ativarAoClick(event) {
+  console.log(event.);
+}
+
+ativar.addEventListener('click', ativarAoClick);
+
+console.log(ativar)
