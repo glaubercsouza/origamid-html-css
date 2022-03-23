096cf9873864ba27280c4f18dc1410e6f8b497e4
glauber_costa14@yahoo.com.br <73006247+glaubercsouza@users.noreply.github.com>
2022-03-22 21:03:04 -0300

    function

---
+++ aulas/javascript/1105-function/script.js
@@ -0,0 +1,21 @@
+// function logCurso(nome, horas) {
+//   console.log(nome, horas, "horas");
+//   return {
+//     nome,
+//     horas
+//   };
+// }
+
+// // logCurso("JavaScript", "40");
+// const retorno = logCurso("HTML", "20");
+
+// console.log(retorno);
+
+const nome = "JavaScript";
+
+function logCurso() {
+  const nome = "HTML";
+  console.log(nome);
+}
+
+logCurso();
