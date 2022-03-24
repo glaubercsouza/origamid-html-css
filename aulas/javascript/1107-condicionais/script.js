d0a1f1d53660ee6d54bd633c107266dc7c819944
glauber_costa14@yahoo.com.br <73006247+glaubercsouza@users.noreply.github.com>
2022-03-24 19:34:35 -0300

    condicional

---
+++ aulas/javascript/1107-condicionais/script.js
@@ -0,0 +1,12 @@
+const elemento = document.querySelector("p");
+const texto = elemento.innerText;
+
+console.log(texto);
+
+if (texto === 'html') {
+  console.log("Executar código")
+}
+
+if (10 > 5) {
+  console.log("teste")
+}
